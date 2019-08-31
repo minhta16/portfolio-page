5ced19560958b326933ee696e41c24f65896ba35
Minh <minhta16@augustana.edu>
2019-08-31 12:41:43 +0700

    eject react app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from '../serviceWorker';
+import App from 'components/App';
+import * as serviceWorker from './serviceWorker';
 import 'assets/styles/bootstrap/css/bootstrap.min.css';
 import 'assets/styles/fontawesome-free/css/all.min.css';
 import 'assets/styles/agency.min.css';
