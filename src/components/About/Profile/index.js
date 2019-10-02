299bf86fda2500c13efc52264e6b7b67a37f5ef6
Minh <minhta16@augustana.edu>
2019-10-02 13:51:14 +1000

    update profile page

--- src/components/About/Profile/index.js
+++ src/components/About/Profile/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import FadeIn from 'react-fade-in';
-import img1 from 'assets/img/team/2.jpg';
+import img1 from 'assets/img/team/3.jpg';
 
 class Profile extends Component {
   render() {
@@ -23,15 +23,26 @@ class Profile extends Component {
           <section className="bg-white page-section" id="team">
             <div className="container">
               <div className="row">
-                <div className="col-sm-12">
+                <div className="col-sm-3">
                   <div className="team-member">
-                    <img className="mx-auto rounded-circle" src={img1} alt="Minh Ta avatar" />
+                    <img className="mx-auto" src={img1} alt="Minh Ta avatar" />
                   </div>
                 </div>
-              </div>
-              <div className="row">
-                <div className="col-lg-8 mx-auto">
-                  <p className="large text-dark align-justify">
+                <div className="col-lg-7 col-lg-offset-2 mx-auto">
+                  <p className="mt-text-paragraph-bold mt-margin-btm-75 align-justify">
+                    A little bit about myself
+                  </p>
+                  <p className="mt-text-paragraph align-justify">
+                    <strong>Home: </strong>
+                    Hanoi, Vietnam
+                    <br />
+                    <strong>Base: </strong>
+                    Rock Island, Illinois, U.S.
+                    <br />
+                    <strong>Favs: </strong>
+                    Photoshop, Reactjs, Java
+                  </p>
+                  <p className="mt-text-paragraph align-justify">
                     Hello, I&apos;m Minh! I am an aspiring senior from Augustana College who is
                     majoring in Computer Science and Applied Mathematics. I am interested in
                     learning software engineering, reading about cutting-edge technology, and
@@ -39,13 +50,13 @@ class Profile extends Component {
                     internship with Got It, Inc. Vietnam and am training myself to become a
                     competent full-stack software developer.
                   </p>
-                  <p className="large text-dark align-justify">
+                  <p className="mt-text-paragraph align-justify">
                     As a prospective developer, I am fascinated with how technology is changing
                     rapidly and how it creates endless potential for solving our problems. I am very
                     thrilled to learn more about Big Data, Artificial Intelligence, and Internet of
                     Things so that I can help making our life a little more convenient.
                   </p>
-                  <p className="large text-dark align-justify">
+                  <p className="mt-text-paragraph align-justify">
                     In my free time, I enjoy taking photographs with my Minolta, hitting notes with
                     my guitar, and checking out amusing landmarks around the world.
                   </p>
